f96b18b4ee47a1ace4be81ceeaae7a28e999a2b5
Matheus0liveiras <matheusfilho98.ms@gmail.com>
2023-07-17 20:29:06 -0300

    feat: create login page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/app_controller.dart';
 import 'package:hello_world/home_page.dart';
+import 'package:hello_world/login_page.dart';
 
 class AppWidget extends StatelessWidget {
   final String title;
@@ -18,7 +19,7 @@ class AppWidget extends StatelessWidget {
                   brightness: AppController.instance.isDarkTheme
                       ? Brightness.dark
                       : Brightness.light),
-              home: HomePage(title: "Clicked"));
+              home: const LoginPage());
         });
   }
 }
---
+++ lib/login_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String emailForm = '';
+  String passwordForm = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      onChanged: (text) {
+                        emailForm = text;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextField(
+                      onChanged: (text) {
+                        passwordForm = text;
+                      },
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (emailForm == 'matheus@mail.com' &&
+                            passwordForm == '123') {
+                          print('SHOW DE BOLA');
+                        }
+                      },
+                      child: const Text('Entrar'),
+                    )
+                  ],
+                ),
+              ))),
+    );
+  }
+}
