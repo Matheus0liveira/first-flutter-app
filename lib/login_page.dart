2ae136e18dd7d25ac9c05bfbab6f5286092f4043
Matheus0liveiras <matheusfilho98.ms@gmail.com>
2023-07-18 21:36:51 -0300

    feat: learn stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,58 +12,73 @@ class _LoginPageState extends State<LoginPage> {
   String emailForm = '';
   String passwordForm = '';
 
+  _body() {
+    return SingleChildScrollView(
+        child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset('assets/images/logo.png'),
+                  ),
+                  const SizedBox(height: 40),
+                  TextField(
+                    onChanged: (text) {
+                      emailForm = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    onChanged: (text) {
+                      passwordForm = text;
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (emailForm == 'matheus@mail.com' &&
+                          passwordForm == '123') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      }
+                    },
+                    child: const Text('Entrar'),
+                  )
+                ],
+              ),
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-          child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Image.asset('assets/images/logo.png'),
-                    ),
-                    const SizedBox(height: 40),
-                    TextField(
-                      onChanged: (text) {
-                        emailForm = text;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        labelText: "Email",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      onChanged: (text) {
-                        passwordForm = text;
-                      },
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: "Password",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 24),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (emailForm == 'matheus@mail.com' &&
-                            passwordForm == '123') {
-                          Navigator.of(context).pushReplacementNamed('/home');
-                        }
-                      },
-                      child: const Text('Entrar'),
-                    )
-                  ],
-                ),
-              ))),
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset('assets/images/bg.avif', fit: BoxFit.cover),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.3),
+          ),
+          _body()
+        ],
+      ),
     );
   }
 }
